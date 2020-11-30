a5b6365c48b598543ece824143ee8b3b66bcbf99
santhosh <68135777+santhosh456s@users.noreply.github.com>
2020-11-30 18:22:28 +0530

    relatives

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,18 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
-
+import React, { Component, useState } from "react";
+import "../styles/App.css";
+const [relatives, setrelatives] = ("santhosh", "shamanth", "shreesha");
 class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="main">
+        <ol key="relativeList">
+          {relatives.map((relative, index) => (
+            <li key="relativeListItem">{relative}</li>
+          ))}
+        </ol>
+      </div>
+    );
+  }
 }
-
 
 export default App;
